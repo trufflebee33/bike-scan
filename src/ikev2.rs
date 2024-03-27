d9d2bc12d47c9abd0358fbfb853eee6613a80085
trufflebee33 <mboettger@trufflepig-forensics.de>
2024-03-27 17:12:30 +0100

    changed ikev2 module and toml

--- src/ikev2.rs
+++ src/ikev2.rs
@@ -1,3 +1,4 @@
+use zerocopy::network_endian::U128;
 use zerocopy::network_endian::U16;
 use zerocopy::network_endian::U32;
 use zerocopy::network_endian::U64;
@@ -14,28 +15,162 @@ pub struct IkeV2 {
     pub header: IkeV2Header,
     pub sa_payload_v2: SecurityAssociationV2,
     pub proposal_v2: Proposal,
-    pub transform: Vec<TransformWrapperV2>,
+    pub encryption_transforms: Vec<TransformAttributeV2>,
+    pub prf_transform: Vec<TransformV2>,
+    pub integrity_algorithm_transform: Vec<TransformV2>,
+    pub diffie_transform: Vec<TransformV2>,
+    pub key_exchange: KeyExchangePayloadV2,
+    pub key_exchange_date: U128,
+    pub nonce_payload: NoncePayloadV2,
+    pub nonce_data: U64,
 }
 //todo(Attribute impln)
 impl IkeV2 {
-    fn build_transforms_v2(&mut self) {
-        let mut transform_vec_v2 = vec![];
-        for encryption_v2 in (1..=9).chain(11..=16).chain(18..=35) {
-            transform_vec_v2.push(TransformWrapperV2 {
-                transform: TransformV2 {
-                    next_transform: 0,
+    pub fn build_transforms_v2() -> (
+        Vec<TransformAttributeV2>,
+        Vec<TransformV2>,
+        Vec<TransformV2>,
+        Vec<TransformV2>,
+    ) {
+        let mut transform_vec_encryption: Vec<TransformAttributeV2> = vec![];
+        let mut transform_vec_prf: Vec<TransformV2> = vec![];
+        let mut transform_vec_integrity_algorithm: Vec<TransformV2> = vec![];
+        let mut transform_vec_diffie_group: Vec<TransformV2> = vec![];
+        for encryption_v2 in (1u16..=9).chain(11..=16).chain(18..=35) {
+            if encryption_v2 == 12 || encryption_v2 == 13 {
+                for attribute_value in [128, 192, 256] {
+                    transform_vec_encryption.push(TransformAttributeV2 {
+                        next_transform: 3,
+                        reserved: 0,
+                        length: Default::default(),
+                        transform_type: u8::from(TransformTypeValues::EncryptionAlgorithm),
+                        reserved2: 0,
+                        transform_id: U16::from(encryption_v2),
+                        attribute: AttributeV2 {
+                            attribute_type: U16::from(AttributeType::KeyLength),
+                            attribute_value: U16::from(attribute_value),
+                        },
+                    });
+                }
+            } else {
+                transform_vec_encryption.push(TransformAttributeV2 {
+                    next_transform: 3,
                     reserved: 0,
                     length: Default::default(),
                     transform_type: u8::from(TransformTypeValues::EncryptionAlgorithm),
                     reserved2: 0,
-                    transform_id: encryption_v2,
-                },
-                attribute: AttributeV2 {
-                    attribute_type: Default::default(),
-                    attribute_value: Default::default(),
-                },
+                    transform_id: U16::from(encryption_v2),
+                    attribute: AttributeV2 {
+                        attribute_type: U16::from(AttributeType::KeyLength),
+                        attribute_value: U16::from(0),
+                    },
+                })
+            }
+        }
+        for prf_value in 1u16..=9 {
+            transform_vec_prf.push(TransformV2 {
+                next_transform: 3,
+                reserved: 0,
+                length: Default::default(),
+                transform_type: u8::from(TransformTypeValues::PseudoRandomFunction),
+                reserved2: 0,
+                transform_id: U16::from(prf_value),
             })
         }
+        for integrity_algorithm in 1u16..=14 {
+            transform_vec_integrity_algorithm.push(TransformV2 {
+                next_transform: 3,
+                reserved: 0,
+                length: Default::default(),
+                transform_type: u8::from(TransformTypeValues::IntegrityAlgorithm),
+                reserved2: 0,
+                transform_id: U16::from(integrity_algorithm),
+            })
+        }
+        for diffie_group in (1u16..=2).chain(5..=5).chain(14..=34) {
+            transform_vec_diffie_group.push(TransformV2 {
+                next_transform: 3,
+                reserved: 0,
+                length: Default::default(),
+                transform_type: u8::from(TransformTypeValues::DiffieHellmanGroup),
+                reserved2: 0,
+                transform_id: U16::from(diffie_group),
+            })
+        }
+        (
+            transform_vec_encryption,
+            transform_vec_prf,
+            transform_vec_integrity_algorithm,
+            transform_vec_diffie_group,
+        )
+    }
+
+    pub fn set_transforms_v2(
+        &mut self,
+        encryption: &[TransformAttributeV2],
+        prf: &[TransformV2],
+        integrity_algorithm: &[TransformV2],
+        diffie_group: &[TransformV2],
+    ) {
+        let full_length =
+            encryption.len() + prf.len() + integrity_algorithm.len() + diffie_group.len();
+        let length_checked = u8::try_from(full_length).expect("Too many transforms");
+        self.proposal_v2.number_of_transforms = length_checked;
+        self.encryption_transforms = Vec::from(encryption);
+        self.prf_transform = Vec::from(prf);
+        self.integrity_algorithm_transform = Vec::from(integrity_algorithm);
+        let mut change_transform = Vec::from(diffie_group);
+        change_transform[diffie_group.len() - 1].next_transform =
+            u8::from(PayloadTypeV2::KeyExchange);
+        self.diffie_transform = change_transform
+    }
+
+    pub fn calculate_length_v2(&mut self) {
+        let mut length = U16::from(0);
+        for encr in &mut self.encryption_transforms {
+            encr.calculate_length();
+            length += encr.length
+        }
+        for prf in &mut self.prf_transform {
+            prf.calculate_length();
+            length += prf.length
+        }
+        for integ_alg in &mut self.integrity_algorithm_transform {
+            integ_alg.calculate_length();
+            length += integ_alg.length
+        }
+        for diffie in &mut self.diffie_transform {
+            diffie.calculate_length();
+            length += diffie.length;
+        }
+        println!("{:?}", length);
+        println!("ecnryption length {}", self.encryption_transforms.len());
+        let proposal_length = U16::from(8) + length;
+        self.proposal_v2.length = proposal_length;
+        println!("proposal length is {:?}", proposal_length);
+        let sa_length = proposal_length;
+        self.sa_payload_v2.sa2_length = sa_length;
+        println!("Sa length is {:?}", sa_length);
+        self.key_exchange.length = U16::from(8);
+        self.nonce_payload.length = U16::from(24);
+        self.header.length = U32::from(28) + U32::from(sa_length);
+        println!("Packet length is {:?}", self.header.length);
+    }
+
+    pub fn convert_to_bytes_v2(&mut self) -> Vec<u8> {
+        let mut bytes_v2 = vec![];
+        bytes_v2.extend_from_slice(self.header.as_bytes());
+        bytes_v2.extend_from_slice(self.sa_payload_v2.as_bytes());
+        bytes_v2.extend_from_slice(self.proposal_v2.as_bytes());
+        bytes_v2.extend_from_slice(self.encryption_transforms.as_bytes());
+        bytes_v2.extend_from_slice(self.prf_transform.as_bytes());
+        bytes_v2.extend_from_slice(self.integrity_algorithm_transform.as_bytes());
+        bytes_v2.extend_from_slice(self.diffie_transform.as_bytes());
+        bytes_v2.extend_from_slice(self.key_exchange.as_bytes());
+        bytes_v2.extend_from_slice(self.key_exchange_date.as_bytes());
+        bytes_v2.extend_from_slice(self.nonce_payload.as_bytes());
+        bytes_v2.extend_from_slice(self.nonce_data.as_bytes());
+        bytes_v2
     }
 }
 
@@ -45,7 +180,7 @@ impl IkeV2 {
 pub struct IkeV2Header {
     pub initiator_spi: U64,
     pub responder_spi: u64,
-    pub next_payload: PayloadTypeV2,
+    pub next_payload: u8,
     pub version: u8,
     pub exchange_type: u8,
     pub flag: u8,
@@ -57,6 +192,7 @@ pub struct IkeV2Header {
 #[derive(Debug, Copy, Clone, AsBytes)]
 #[repr(u8)]
 pub enum PayloadTypeV2 {
+    NoNextPayload,
     SecurityAssociation,
     KeyExchange,
     IdentificationInitiator,
@@ -76,6 +212,7 @@ pub enum PayloadTypeV2 {
 impl From<PayloadTypeV2> for u8 {
     fn from(value: PayloadTypeV2) -> Self {
         match value {
+            PayloadTypeV2::NoNextPayload => 0,
             PayloadTypeV2::SecurityAssociation => 33,
             PayloadTypeV2::KeyExchange => 34,
             PayloadTypeV2::IdentificationInitiator => 35,
@@ -97,6 +234,7 @@ impl From<PayloadTypeV2> for u8 {
 impl PayloadTypeV2 {
     fn try_from_u8(value: u8) -> Option<Self> {
         match value {
+            0 => Some(PayloadTypeV2::NoNextPayload),
             33 => Some(PayloadTypeV2::SecurityAssociation),
             34 => Some(PayloadTypeV2::KeyExchange),
             35 => Some(PayloadTypeV2::IdentificationInitiator),
@@ -153,10 +291,9 @@ impl ExchangeTypeV2 {
 #[derive(Debug, Copy, Clone, AsBytes)]
 #[repr(packed)]
 pub struct SecurityAssociationV2 {
-    sa2_next_payload: PayloadTypeV2,
-    critical_bit: u8,
-    sa2_reserved: u8,
-    sa2_length: U16,
+    pub sa2_next_payload: u8,
+    pub critical_bit: u8,
+    pub sa2_length: U16,
 }
 
 ///Proposal IkeV2 RFC 7296 page 80
@@ -212,7 +349,7 @@ impl ProtocolId {
 }
 
 ///Transform Payload for IkeV2 Rfc 7296 page 79
-#[derive(Debug, Copy, Clone, AsBytes)]
+#[derive(Debug, Copy, Clone, AsBytes, PartialEq)]
 #[repr(packed)]
 pub struct TransformV2 {
     pub next_transform: u8,
@@ -220,24 +357,56 @@ pub struct TransformV2 {
     pub length: U16,
     pub transform_type: u8,
     pub reserved2: u8,
-    pub transform_id: u8,
+    pub transform_id: U16,
+}
+
+impl TransformV2 {
+    pub fn calculate_length(&mut self) {
+        self.length = U16::from(8);
+    }
 }
 
 ///Wrapper struct for transforms
-#[derive(Debug, Copy, Clone, AsBytes)]
+#[derive(Debug, Copy, Clone, AsBytes, PartialEq)]
 #[repr(packed)]
-pub struct TransformWrapperV2 {
-    pub transform: TransformV2,
+pub struct TransformAttributeV2 {
+    pub next_transform: u8,
+    pub reserved: u8,
+    pub length: U16,
+    pub transform_type: u8,
+    pub reserved2: u8,
+    pub transform_id: U16,
     pub attribute: AttributeV2,
 }
 
+impl TransformAttributeV2 {
+    pub fn calculate_length(&mut self) {
+        self.length = U16::from(4 + 8);
+    }
+}
+
 ///Key Length Attribute
-#[derive(Debug, Copy, Clone, AsBytes, FromBytes, FromZeroes)]
+#[derive(Debug, Copy, Clone, AsBytes, FromBytes, FromZeroes, PartialEq)]
 #[repr(packed)]
 pub struct AttributeV2 {
     pub attribute_type: U16,
     pub attribute_value: U16,
 }
+
+#[derive(Debug, Copy, Clone, AsBytes)]
+#[repr(u8)]
+pub enum AttributeType {
+    KeyLength,
+}
+
+impl From<AttributeType> for U16 {
+    fn from(value: AttributeType) -> Self {
+        Self::new(match value {
+            AttributeType::KeyLength => 14 | 1 << 15,
+        })
+    }
+}
+
 ///key length of AES_CBC and AES_CTR
 #[derive(Debug, Copy, Clone, AsBytes)]
 #[repr(u8)]
@@ -279,4 +448,25 @@ impl From<TransformTypeValues> for u8 {
             TransformTypeValues::ExtendedSequenceNumbers => 5,
         }
     }
+}
+
+///Key Exchange Payload RFC page 89
+#[derive(Debug, Copy, Clone, AsBytes, FromBytes, FromZeroes, PartialEq)]
+#[repr(packed)]
+pub struct KeyExchangePayloadV2 {
+    pub next_payload: u8,
+    pub critical_bit: u8,
+    pub reserved: u8,
+    pub length: U16,
+    pub diffie_hellman_group: U32,
+    pub reserved2: U32,
+}
+///Nonce Payload RFC 7296 page 99
+#[derive(Debug, Copy, Clone, AsBytes, FromBytes, FromZeroes, PartialEq)]
+#[repr(packed)]
+pub struct NoncePayloadV2 {
+    pub next_payload_: u8,
+    pub critical_bit: u8,
+    pub reserved: u8,
+    pub length: U16,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,11 @@ use std::net::SocketAddr;
 use std::thread::sleep;
 use std::time;
 
+use rand::distributions::Alphanumeric;
+use rand::random;
 use rand::Rng;
 use tokio::net::UdpSocket;
+use zerocopy::network_endian::U128;
 use zerocopy::network_endian::U32;
 use zerocopy::network_endian::U64;
 use zerocopy::AsBytes;
@@ -16,6 +19,16 @@ use crate::ike::PayloadTypeV1::NoNextPayload;
 use crate::ike::PayloadTypeV1::SecurityAssociation;
 use crate::ike::ProposalPayload;
 use crate::ike::SecurityAssociationV1;
+use crate::ikev2::ExchangeTypeV2;
+use crate::ikev2::IkeV2;
+use crate::ikev2::IkeV2Header;
+use crate::ikev2::KeyExchangePayloadV2;
+use crate::ikev2::NoncePayloadV2;
+use crate::ikev2::PayloadTypeV2;
+use crate::ikev2::Proposal;
+use crate::ikev2::ProtocolId;
+use crate::ikev2::SecurityAssociationV2;
+use crate::ikev2::TransformTypeValues::DiffieHellmanGroup;
 use crate::parse_ike::ResponsePacket;
 
 pub mod ike;
@@ -26,8 +39,8 @@ pub async fn scan() -> io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0".parse::<SocketAddr>().unwrap()).await?;
     let remote_addr = "192.168.122.68:500".parse::<SocketAddr>().unwrap();
     socket.connect(remote_addr).await?;
-
-    //sendng packet
+    /**
+    //sending IKE Version 1 packet
     let transforms = IkeV1::build_transforms();
     for chunk in transforms.chunks(255) {
         //calculate random Initiator Security Parameter Index
@@ -83,6 +96,81 @@ pub async fn scan() -> io::Result<()> {
         ike_response.parse_response();
         let seconds = time::Duration::from_millis(1360);
         tokio::time::sleep(seconds).await;
+    }*/
+    //sending IKE Version 2 Packet
+    let transforms_v2 = IkeV2::build_transforms_v2();
+    let key_data: u128 = rand::thread_rng().gen();
+    for encryption_chunk in transforms_v2.0.chunks(63) {
+        for prf_chunk in transforms_v2.1.chunks(63) {
+            for integrity_algorithm_chunk in transforms_v2.2.chunks(63) {
+                for diffie_group_chunk in transforms_v2.3.chunks(63) {
+                    let initiator_spi_v2: u64 = rand::thread_rng().gen();
+                    let nonce: u64 = rand::thread_rng().gen();
+                    let mut ike_v2 = IkeV2 {
+                        header: IkeV2Header {
+                            initiator_spi: U64::from(initiator_spi_v2),
+                            responder_spi: 0,
+                            next_payload: u8::from(PayloadTypeV2::SecurityAssociation),
+                            version: 32,
+                            exchange_type: u8::from(ExchangeTypeV2::IkeSaInit),
+                            flag: 8,
+                            message_id: 0,
+                            length: Default::default(),
+                        },
+                        sa_payload_v2: SecurityAssociationV2 {
+                            sa2_next_payload: u8::from(PayloadTypeV2::KeyExchange),
+                            critical_bit: 0,
+                            sa2_length: Default::default(),
+                        },
+                        proposal_v2: Proposal {
+                            next_proposal: 0,
+                            reserved: 0,
+                            length: Default::default(),
+                            proposal_number: 1,
+                            protocol_id: ProtocolId::IKE,
+                            spi_size: 0,
+                            number_of_transforms: Default::default(),
+                        },
+                        encryption_transforms: vec![],
+                        prf_transform: vec![],
+                        integrity_algorithm_transform: vec![],
+                        diffie_transform: vec![],
+                        key_exchange: KeyExchangePayloadV2 {
+                            next_payload: u8::from(PayloadTypeV2::Nonce),
+                            critical_bit: 0,
+                            reserved: 0,
+                            length: Default::default(),
+                            diffie_hellman_group: U32::from(14),
+                            reserved2: Default::default(),
+                        },
+                        key_exchange_date: U128::from(key_data),
+                        nonce_payload: NoncePayloadV2 {
+                            next_payload_: 0,
+                            critical_bit: 0,
+                            reserved: 0,
+                            length: Default::default(),
+                        },
+                        nonce_data: U64::from(nonce),
+                    };
+                    ike_v2.set_transforms_v2(
+                        encryption_chunk,
+                        prf_chunk,
+                        integrity_algorithm_chunk,
+                        diffie_group_chunk,
+                    );
+                    ike_v2.calculate_length_v2();
+
+                    let bytes_v2 = ike_v2.convert_to_bytes_v2();
+                    socket.send(&bytes_v2).await.expect("Couldn't send packet");
+
+                    let mut buf = [0u8; 112];
+                    socket
+                        .recv_from(&mut buf)
+                        .await
+                        .expect("couldn't read buffer");
+                }
+            }
+        }
     }
     Ok(())
 }
