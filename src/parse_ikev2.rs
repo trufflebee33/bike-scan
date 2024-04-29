7374cd9d515f6c1b7d1db1e4ddabb01399c08ac9
trufflebee33 <mboettger@trufflepig-forensics.de>
2024-04-29 14:20:19 +0200

    added documentation for parse_ikev2 module

--- src/parse_ikev2.rs
+++ src/parse_ikev2.rs
@@ -1,3 +1,6 @@
+//! # Bike-Scan
+//! das folgende Modul wird zum Parsen von Ike Version 2 verwendet
+
 use zerocopy::network_endian::U128;
 use zerocopy::network_endian::U16;
 use zerocopy::network_endian::U32;
@@ -7,106 +10,157 @@ use zerocopy::FromBytes;
 use zerocopy::FromZeroes;
 
 use crate::ikev2::ProtocolId;
-
+///Wrapper Struct für das Parsen des Ike Version 2 Protokolls
 #[derive(Debug, Clone, Copy, FromBytes, FromZeroes)]
 #[repr(packed)]
 pub struct ResponsePacketV2 {
+    ///Ike Header
     pub header: ResponseHeaderV2,
+    ///Security Association Payload
     pub sa_payload_v2: ResponseSecurityAssociationV2,
+    ///Proposal Payload
     pub proposal_v2: ResponseProposalV2,
-    pub encryption_transform: ResponseTransformAttributeV2,
+    ///Verschlüsselungsalgorithmus
+    pub encryption_transform: ResponseTransformV2,
+    ///Pseudo-Random Funktion
     pub prf_transform: ResponseTransformV2,
+    ///Integritätslagorithmus
     pub integrity_algorithm_transform: ResponseTransformV2,
+    ///Diffie-Helman Gruppe
     pub diffie_transform: ResponseTransformV2,
 }
 
 impl ResponsePacketV2 {
+    ///Parsen der Antwort des Servers.
+    /// Die Funktion read_from_prefix liest den ersten Teil der Antwort aus.
+    /// Die Länge des Wrapperstruct darf nicht größer als die Länge der
+    /// gesendeten Bytes sein.
     pub fn parse_ike_v2(buf: &[u8]) -> Option<Self> {
         Self::read_from_prefix(buf)
     }
 }
 
-#[derive(Debug, Clone, Copy, FromBytes, FromZeroes)]
-#[repr(packed)]
-pub struct ParsingStruct {
-    pub header: ResponseHeaderV2,
-    pub sa_payload_v2: ResponseSecurityAssociationV2,
-    pub proposal_v2: ResponseProposalV2,
-    pub encryption_transform: ResponseTransformAttributeV2,
-    pub prf_transform: ResponseTransformV2,
-    pub integrity_algorithm_transform: ResponseTransformV2,
-    pub diffie_transform: ResponseTransformV2,
-}
+///Ike-Header
 #[derive(Debug, Clone, Copy, FromBytes, FromZeroes)]
 #[repr(packed)]
 pub struct ResponseHeaderV2 {
+    ///Security Parameter Index des Initiators
     pub initiator_spi: U64,
+    ///Security Parameter Index des Responder
+    /// bekommt den Wert 0
     pub responder_spi: U64,
+    ///nächster Payload
     pub next_payload: u8,
+    ///Ike Version
     pub version: u8,
+    ///Austausch Typ
     pub exchange_type: u8,
+    ///Flags
+    /// sind erst in der zweiten Phase notwendig
+    /// und können den Wert 0 bekommen
     pub flag: u8,
-    pub message_id: u32,
+    ///Nachrichten ID
+    pub message_id: U32,
+    ///Länge des Ike Pakets
     pub length: U32,
 }
 
+///Security Association Payload der Antwort
 #[derive(Debug, Clone, Copy, FromBytes, FromZeroes)]
 #[repr(packed)]
 pub struct ResponseSecurityAssociationV2 {
+    ///nächster Payload
     pub sa2_next_payload: u8,
+    ///kritisches Bit
     pub critical_bit: u8,
+    ///Länge des Payloads
     pub sa2_length: U16,
 }
 
+///Proposal der Antwort
 #[derive(Debug, Clone, Copy, FromBytes, FromZeroes)]
 #[repr(packed)]
 pub struct ResponseProposalV2 {
+    ///nächstes Proposal
     pub next_proposal: u8,
+    ///reservierter Bereich
     pub reserved: u8,
+    ///Länge des Proposals
     pub length: U16,
+    ///Nummer des Proposals
     pub proposal_number: u8,
+    ///ID des verwendeten Protokolls (IKE)
     pub protocol_id: u8,
+    ///Größe des Security Parameter Indexes
     pub spi_size: u8,
+    ///Anzahl der Transformationen
     pub number_of_transforms: u8,
 }
 
+///Transformation der Antwort, ohne Attribut.
+/// Dieses Struct wird für den Integritätsalgortihmus,
+/// die Pseudo-Random Funktion und die Diffie-Hellman Gruppe verwendet
 #[derive(Debug, Clone, Copy, FromBytes, FromZeroes)]
 #[repr(packed)]
 pub struct ResponseTransformV2 {
+    ///nächste Transformation
     pub next_transform: u8,
+    ///reserviertere Bereich
     pub reserved: u8,
+    ///Länge der Transformation
     pub length: U16,
+    ///Typ der Transformation
     pub transform_type: u8,
+    ///zweiter reservierter Bereich
     pub reserved2: u8,
+    ///Transformations ID
     pub transform_id: U16,
 }
 
+///Transformation der Antwort mit Attribut.
+/// Dieses Struct wird für das Parsen des Verschlüsselungsalgorithmus verwendet.
 #[derive(Debug, Clone, Copy, FromBytes, FromZeroes)]
 #[repr(packed)]
 pub struct ResponseTransformAttributeV2 {
+    ///nächste Transformation
     pub next_transform: u8,
+    ///reserviertere Bereich
     pub reserved: u8,
+    ///Länge der Transformation
     pub length: U16,
+    ///Typ der Transformation
     pub transform_type: u8,
+    ///zweiter reservierter Bereich
     pub reserved2: u8,
+    ///Transformations ID
     pub transform_id: U16,
+    ///Attribut für die Schlüssellänge
     pub attribute: ResponseAttributeV2,
 }
 
+///Attribut zum Verarbeiten der Schlüssellänge
 #[derive(Debug, Clone, Copy, FromBytes, FromZeroes)]
 #[repr(packed)]
 pub struct ResponseAttributeV2 {
+    ///Attribut Typ = Schlüssellänge
     pub attribute_type: U16,
+    ///Attributwert = 128, 192 oder 256 Bit
     pub attribute_value: U16,
 }
 
+///Key Exchange Payload der Antwort
 #[derive(Debug, Clone, Copy, FromBytes, FromZeroes)]
 #[repr(packed)]
 pub struct ResponseKeyExchangePayloadV2 {
+    ///nächster Payload
     pub next_payload: u8,
+    ///reservierter Bereich
     pub reserved: u8,
+    ///Länge des Payloads
     pub length: U16,
+    ///Diffie-Hellman Gruppe des Servers
     pub diffie_hellman_group: U16,
+    ///Zweiter reservierter Bereich
     pub reserved2: U16,
 }
 ///Nonce Payload (RFC 7296 Seite 99)
