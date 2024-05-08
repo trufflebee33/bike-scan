85651ffce039226962db5e5c051ab0f56ec34667
trufflebee33 <mboettger@trufflepig-forensics.de>
2024-05-08 12:38:29 +0200

    changed parsing for ikev2 to handel notify message

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,7 @@ use crate::ike::PayloadTypeV1::NoNextPayload;
 use crate::ike::PayloadTypeV1::SecurityAssociation;
 use crate::ike::ProposalPayload;
 use crate::ike::SecurityAssociationV1;
+use crate::ikev2::testversion::TestIkeVersion;
 use crate::ikev2::ExchangeTypeV2;
 use crate::ikev2::IkeV2;
 use crate::ikev2::IkeV2Header;
@@ -36,8 +37,8 @@ use crate::ikev2::PayloadTypeV2;
 use crate::ikev2::Proposal;
 use crate::ikev2::ProtocolId;
 use crate::ikev2::SecurityAssociationV2;
-use crate::ikev2::TestIkeVersion;
 use crate::parse_ike::ResponsePacket;
+use crate::parse_ikev2::NotifyPacket;
 use crate::parse_ikev2::ResponsePacketV2;
 
 pub mod ike;
@@ -51,7 +52,7 @@ pub mod parse_ikev2;
 /// Die Antworten des Servers werden für IkeV1 verarbeitet.
 pub async fn scan() -> io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0".parse::<SocketAddr>().unwrap()).await?;
-    let remote_addr = "192.168.33.10:500".parse::<SocketAddr>().unwrap();
+    let remote_addr = "ip:500".parse::<SocketAddr>().unwrap();
     socket.connect(remote_addr).await?;
     //sending IKE Version 1 packet
     let transforms = IkeV1::build_transforms();
@@ -91,6 +92,7 @@ pub async fn scan() -> io::Result<()> {
         ike_v1.set_transforms(chunk);
         ike_v1.calculate_length();
         let bytes = ike_v1.convert_to_bytes();
+        println!("Sende Paket");
 
         socket.send(&bytes).await.expect("Couldn't send packet");
 
@@ -105,7 +107,7 @@ pub async fn scan() -> io::Result<()> {
         //parse Ike Response
         let ike_response = ResponsePacket::read_from_prefix(byte_slice).expect("Slice too short");
         ike_response.parse_response();
-        let seconds = time::Duration::from_secs(60);
+        let seconds = time::Duration::from_secs(90);
         tokio::time::sleep(seconds).await;
     }
     Ok(())
@@ -117,109 +119,118 @@ pub async fn scan_v2() -> io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0".parse::<SocketAddr>().unwrap()).await?;
     let remote_addr = "192.168.33.10:500".parse::<SocketAddr>().unwrap();
     socket.connect(remote_addr).await?;
-
-    //test paket
-    let check_version = TestIkeVersion::build_test_packet();
-    println!("{:?}", check_version);
-    socket
-        .send(&check_version)
-        .await
-        .expect("Couldn't send packet");
-
-    let mut buf_v2 = [0u8; 285];
-    socket
-        .recv_from(&mut buf_v2)
-        .await
-        .expect("couldn't read buffer");
-
-    /*
     //sending IKE Version 2 Packet
     let transforms_v2 = IkeV2::build_transforms_v2();
     for encryption_chunk in transforms_v2.0.chunks(63) {
         for prf_chunk in transforms_v2.1.chunks(63) {
             for integrity_algorithm_chunk in transforms_v2.2.chunks(63) {
                 for diffie_group_chunk in transforms_v2.3.chunks(63) {
-                    let initiator_spi_v2: u64 = rand::thread_rng().gen();
-                    let mut ike_v2 = IkeV2 {
-                        header: IkeV2Header {
-                            initiator_spi: U64::from(initiator_spi_v2),
-                            responder_spi: U64::from(0),
-                            next_payload: u8::from(PayloadTypeV2::SecurityAssociation),
-                            version: 32,
-                            exchange_type: u8::from(ExchangeTypeV2::IkeSaInit),
-                            flag: 8,
-                            message_id: 0,
-                            length: Default::default(),
-                        },
-                        sa_payload_v2: SecurityAssociationV2 {
-                            sa2_next_payload: u8::from(PayloadTypeV2::KeyExchange),
-                            critical_bit: 0,
-                            sa2_length: Default::default(),
-                        },
-                        proposal_v2: Proposal {
-                            next_proposal: 0,
-                            reserved: 0,
-                            length: Default::default(),
-                            proposal_number: 1,
-                            protocol_id: ProtocolId::IKE,
-                            spi_size: 0,
-                            number_of_transforms: Default::default(),
-                        },
-                        encryption_transforms: vec![],
-                        prf_transform: vec![],
-                        integrity_algorithm_transform: vec![],
-                        diffie_transform: vec![],
-                        key_exchange: KeyExchangePayloadV2 {
-                            next_payload: u8::from(PayloadTypeV2::Nonce),
-                            reserved: 0,
-                            length: Default::default(),
-                            diffie_hellman_group: U16::from(1),
-                            reserved2: Default::default(),
-                        },
-                        key_exchange_data: vec![],
-                        nonce_payload: NoncePayloadV2 {
-                            next_payload_: 0,
-                            reserved: 0,
-                            length: Default::default(),
-                        },
-                        nonce_data: vec![],
-                    };
-                    ike_v2.set_transforms_v2(
-                        encryption_chunk,
-                        prf_chunk,
-                        integrity_algorithm_chunk,
-                        diffie_group_chunk,
-                    );
-                    ike_v2.generate_key_exchange_data();
-                    ike_v2.generate_nonce_data();
-                    ike_v2.calculate_length_v2();
+                    for key_exchange_diffie_group in (1u16..=2).chain(5..=5).chain(14..=18) {
+                        let initiator_spi_v2: u64 = rand::thread_rng().gen();
+                        let mut ike_v2 = IkeV2 {
+                            header: IkeV2Header {
+                                initiator_spi: U64::from(initiator_spi_v2),
+                                responder_spi: U64::from(0),
+                                next_payload: u8::from(PayloadTypeV2::SecurityAssociation),
+                                version: 32,
+                                exchange_type: u8::from(ExchangeTypeV2::IkeSaInit),
+                                flag: 8,
+                                message_id: 0,
+                                length: Default::default(),
+                            },
+                            sa_payload_v2: SecurityAssociationV2 {
+                                sa2_next_payload: u8::from(PayloadTypeV2::KeyExchange),
+                                critical_bit: 0,
+                                sa2_length: Default::default(),
+                            },
+                            proposal_v2: Proposal {
+                                next_proposal: 0,
+                                reserved: 0,
+                                length: Default::default(),
+                                proposal_number: 1,
+                                protocol_id: ProtocolId::IKE,
+                                spi_size: 0,
+                                number_of_transforms: Default::default(),
+                            },
+                            encryption_transforms: vec![],
+                            prf_transform: vec![],
+                            integrity_algorithm_transform: vec![],
+                            diffie_transform: vec![],
+                            key_exchange: KeyExchangePayloadV2 {
+                                next_payload: u8::from(PayloadTypeV2::Nonce),
+                                reserved: 0,
+                                length: Default::default(),
+                                diffie_hellman_group: U16::from(key_exchange_diffie_group),
+                                reserved2: Default::default(),
+                            },
+                            key_exchange_data: vec![],
+                            nonce_payload: NoncePayloadV2 {
+                                next_payload_: 0,
+                                reserved: 0,
+                                length: Default::default(),
+                            },
+                            nonce_data: vec![],
+                        };
+                        ike_v2.set_transforms_v2(
+                            encryption_chunk,
+                            prf_chunk,
+                            integrity_algorithm_chunk,
+                            diffie_group_chunk,
+                        );
+                        ike_v2.generate_key_exchange_data();
+                        ike_v2.generate_nonce_data();
+                        ike_v2.calculate_length_v2();
 
-                    let bytes_v2 = ike_v2.convert_to_bytes_v2();
-                    socket
-                        .send(&bytes_v2)
-                        .await
-                        .expect("Couldn't send packet");
+                        let bytes_v2 = ike_v2.convert_to_bytes_v2();
+                        socket.send(&bytes_v2).await.expect("Couldn't send packet");
 
-                    let mut buf_v2 = [0u8; 285];
-                    socket
-                        .recv_from(&mut buf_v2)
-                        .await
-                        .expect("couldn't read buffer");
-                    let byte_slice_v2 = buf_v2.as_slice();
-                    let ike_v2_response = ResponsePacketV2::parse_ike_v2(byte_slice_v2).unwrap();
-                    //println!("{:?}", ike_v2_response);
+                        let mut buf_v2 = [0u8; 285];
+                        socket
+                            .recv_from(&mut buf_v2)
+                            .await
+                            .expect("couldn't read buffer");
+                        let byte_slice_v2 = buf_v2.as_slice();
+                        let ike_v2_response =
+                            ResponsePacketV2::parse_ike_v2(byte_slice_v2).unwrap();
+                        //println!("{:?}", ike_v2_response);
 
-                    println!(
-                        "Ike Version is {:?}, ExchangeType is {:?}",
-                        ike_v2_response.header.version, ike_v2_response.header.exchange_type
-                    );
+                        println!(
+                            "Ike Version is {:?}, ExchangeType is {:?}",
+                            ike_v2_response.header.version, ike_v2_response.header.exchange_type
+                        );
 
-                    println!("Found Transforms: Encryption Algorthm: {:?}, Prf-Funktion: {:?}, Integrity Algorithm: {:?}, Diffie-Hellamn-Gruppe: {:?}"
+                        println!("Found Transforms: Encryption Algorthm: {:?}, Prf-Funktion: {:?}, Integrity Algorithm: {:?}, Diffie-Hellamn-Gruppe: {:?}"
                              ,ike_v2_response.encryption_transform.transform_id, ike_v2_response.prf_transform.transform_id, ike_v2_response.integrity_algorithm_transform.transform_id,
                              ike_v2_response.diffie_transform.transform_id);
+
+                        if ike_v2_response.header.next_payload == 41 {
+                            let notify_response =
+                                NotifyPacket::parse_notify(byte_slice_v2).unwrap();
+                            println!(
+                                "Notify Message: Error Code {:?}",
+                                notify_response.notify_payload.notify_message_type
+                            )
+                        }
+                    }
                 }
             }
         }
-    }*/
+    }
     Ok(())
 }
+
+/*pub async fn test_version() -> io::Result<()> {
+    let version = TestIkeVersion::build_test_packet();
+    println!("{:?}", version);
+    let socket = UdpSocket::bind("0.0.0.0:0".parse::<SocketAddr>().unwrap()).await?;
+    let remote_addr = "192.168.33.10:500".parse::<SocketAddr>().unwrap();
+    socket.connect(remote_addr).await?;
+    socket.send(&version).await.expect("Couldn't send packet");
+    let mut buf_v2 = [0u8; 50];
+    socket
+        .recv_from(&mut buf_v2)
+        .await
+        .expect("couldn't read buffer");
+
+    Ok(())
+}*/
